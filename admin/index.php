10afe5f2157ccd4d7b7233872ef48b9169fa9e5e
kihajardewabanget <117788873+reno324235@users.noreply.github.com>
2024-02-05 19:20:51 +0700

    Add files via upload

---
+++ admin/index.php
@@ -0,0 +1,174 @@
+<?php 
+	session_start();
+	include '../conn/koneksi.php';
+	if(!isset($_SESSION['username'])){
+		header('location:../index.php');
+	}
+	elseif($_SESSION['data']['level'] != "admin"){
+		header('location:../index.php');
+	}
+ ?>
+  <!DOCTYPE html>
+  <html>
+    <head>
+    	<title>Aplikasi PEDUMAS</title>
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="../css/materialize.min.css"  media="screen,projection"/>
+	  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+      <!-- Compiled and minified CSS -->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+	  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+      <!-- Compiled and minified JavaScript -->
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+      <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
+      
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+      
+      <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
+      
+      
+      <script type="text/javascript">
+        $(document).ready( function () {
+          $('#example').DataTable();
+          $('select').formSelect();
+        } );
+      
+      </script>
+
+    </head>
+
+    <body>
+
+	<div class="row">
+        <div class="col-12 col-md-3">
+            <ul id="slide-out" class="sidenav sidenav-fixed bg-light">
+                <li>
+                    <div class="user-view">
+                        <a href="#user">
+                            <img class="circle img-fluid" src="https://tse1.mm.bing.net/th?id=OIP.7m4zDlGP-kuETYyfoAzx6AAAAA&pid=Api&P=0&w=300&h=300">
+                        </a>
+                        <a href="#name"><span class="blue-text name"><?php echo ucwords($_SESSION['data']['nama_petugas']); ?></span></a>
+                    </div>
+                </li>
+                <!-- Add Bootstrap classes to list items -->
+                <li class="nav-item"><a class="nav-link" href="index.php?p=dashboard"><i class="fas fa-tachometer-alt"></i> Dashboard</a></li>
+                <li class="nav-item"><a class="nav-link" href="index.php?p=registrasi"><i class="fas fa-list-alt"></i> Registrasi</a></li>
+                <li class="nav-item"><a class="nav-link" href="index.php?p=pengaduan"><i class="fas fa-flag"></i> Pengaduan</a></li>
+                <li class="nav-item"><a class="nav-link" href="index.php?p=respon"><i class="fas fa-comment"></i> Respon</a></li>
+                <li class="nav-item"><a class="nav-link" href="index.php?p=user"><i class="fas fa-user"></i> User</a></li>
+                <li class="nav-item"><a class="nav-link" href="index.php?p=laporan"><i class="fas fa-book"></i> Laporan</a></li>
+                <li>
+                    <hr class="dropdown-divider">
+                </li>
+                <li class="nav-item"><a class="nav-link waves-effect" href="../index.php?p=logout"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
+            </ul>
+
+            <button class="btn btn-secondary sidenav-trigger" data-bs-target="#slide-out"><i class="fas fa-bars"></i></button>
+        </div>
+
+        <div class="col-12 col-md-9">
+        
+	<?php 
+		if(@$_GET['p']==""){
+			include_once 'dashboard.php';
+		}
+		elseif(@$_GET['p']=="dashboard"){
+			include_once 'dashboard.php';
+		}
+		elseif(@$_GET['p']=="registrasi"){
+			include_once 'registrasi.php';
+		}
+		elseif(@$_GET['p']=="regis_hapus"){
+			$query = mysqli_query($koneksi,"DELETE FROM masyarakat WHERE nik='".$_GET['nik']."'");
+			if($query){
+				header('location:index.php?p=registrasi');
+			}
+		}
+		elseif(@$_GET['p']=="pengaduan"){
+			include_once 'pengaduan.php';
+		}
+		elseif(@$_GET['p']=="pengaduan_hapus"){
+			$query=mysqli_query($koneksi,"SELECT * FROM pengaduan WHERE id_pengaduan='".$_GET['id_pengaduan']."'");
+			$data=mysqli_fetch_assoc($query);
+			unlink('../img/'.$data['foto']);
+			if($data['status']=="proses"){
+				$delete=mysqli_query($koneksi,"DELETE FROM pengaduan WHERE id_pengaduan='".$_GET['id_pengaduan']."'");
+				header('location:index.php?p=pengaduan');
+			}
+			elseif($data['status']=="selesai"){
+				$delete=mysqli_query($koneksi,"DELETE FROM pengaduan WHERE id_pengaduan='".$_GET['id_pengaduan']."'");
+				if($delete){
+					$delete2=mysqli_query($koneksi,"DELETE FROM tanggapan WHERE id_pengaduan='".$_GET['id_pengaduan']."'");
+					header('location:index.php?p=pengaduan');
+				}	
+			}
+
+		}
+		elseif(@$_GET['p']=="more"){
+			include_once 'more.php';
+		}
+		elseif(@$_GET['p']=="respon"){
+			include_once 'respon.php';
+		}
+		elseif(@$_GET['p']=="tanggapan_hapus"){
+			
+			$query = mysqli_query($koneksi,"DELETE FROM tanggapan WHERE id_tanggapan='".$_GET['id_tanggapan']."'");
+			if($query){
+				header('location:index.php?p=respon');
+			}
+		}
+		elseif(@$_GET['p']=="user"){
+			include_once 'user.php';
+		}
+		elseif(@$_GET['p']=="user_input"){
+			include_once 'user_input.php';
+		}
+		elseif(@$_GET['p']=="user_edit"){
+			include_once 'user_edit.php';
+		}
+		elseif(@$_GET['p']=="user_hapus"){
+			$query = mysqli_query($koneksi,"DELETE FROM petugas WHERE id_petugas='".$_GET['id_petugas']."'");
+			if($query){
+				header('location:index.php?p=user');
+			}
+		}
+		elseif(@$_GET['p']=="laporan"){
+			include_once 'laporan.php';
+		}
+	 ?>
+
+      </div>
+
+
+    </div>
+
+
+
+
+      <!--JavaScript at end of body for optimized loading-->
+      <script type="text/javascript" src="../js/materialize.min.js"></script>
+      <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+      <script type="text/javascript">
+        document.addEventListener('DOMContentLoaded', function() {
+          var elems = document.querySelectorAll('.sidenav');
+          var instances = M.Sidenav.init(elems);
+        });
+
+        document.addEventListener('DOMContentLoaded', function() {
+          var elems = document.querySelectorAll('.modal');
+          var instances = M.Modal.init(elems);
+        });
+
+      </script>
+
+    </body>
+  </html>
